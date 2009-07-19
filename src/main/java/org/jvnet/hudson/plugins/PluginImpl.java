3e870230c42fc46a2420dbae32cedbe4c6a51285
rtyler <rtyler@dev.java.net>
2009-07-19 01:23:04 +0000

    Finish up the basic implementation a Jython plugin, executing Python in ur javas!

--- src/main/java/org/jvnet/hudson/plugins/PluginImpl.java
+++ src/main/java/org/jvnet/hudson/plugins/PluginImpl.java
@@ -4,18 +4,18 @@ import hudson.Plugin;
 import hudson.tasks.BuildStep;
 
 /**
- * Entry point of a plugin.
+ * Jython plugin entry point
  *
  * <p>
  * There must be one {@link Plugin} class in each plugin.
  * See javadoc of {@link Plugin} for more about what can be done on this class.
  *
- * @author Kohsuke Kawaguchi
+ * @author R. Tyler Ballance
  */
 public class PluginImpl extends Plugin {
     public void start() throws Exception {
         // plugins normally extend Hudson by providing custom implementations
         // of 'extension points'. In this example, we'll add one builder.
-        BuildStep.BUILDERS.add(HelloWorldBuilder.DESCRIPTOR);
+        BuildStep.BUILDERS.add(Jython.DESCRIPTOR);
     }
 }
