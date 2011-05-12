ebcbd0a2e594e8979706882d680a1376baed1710
jackgene <jackgene@java.net>
2011-05-11 18:52:04 -0700

    Exposed build variables as environment variables.

--- src/main/java/org/jvnet/hudson/plugins/Jython.java
+++ src/main/java/org/jvnet/hudson/plugins/Jython.java
@@ -1,24 +1,20 @@
 package org.jvnet.hudson.plugins;
 
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
 import hudson.Extension;
 import hudson.Launcher;
-import hudson.Launcher.ProcStarter;
 import hudson.model.AbstractBuild;
 import hudson.model.BuildListener;
 import hudson.model.Descriptor;
-import hudson.model.Hudson;
 import hudson.model.JDK;
 import hudson.model.Node;
 import hudson.model.Result;
-import hudson.remoting.Which;
 import hudson.tasks.Builder;
-import java.io.ByteArrayInputStream;
 import net.sf.json.JSONObject;
 import org.kohsuke.stapler.StaplerRequest;
-
-import java.io.IOException;
-
-import org.python.util.jython;
 
 /**
  * Jython builder.
@@ -32,30 +28,30 @@ import org.python.util.jython;
  */
 public class Jython extends Builder {
     private final String command;
-
+    
     private Jython(String command) {
         this.command = command;
     }
-
+    
     public String getCommand() {
         return command;
     }
-
+    
     @Extension
     public static final class DescriptorImpl extends Descriptor<Builder> {
         public DescriptorImpl() {
             super(Jython.class);
         }
-
+        
         @Override
         public Builder newInstance(StaplerRequest req, JSONObject formData) {
             return new Jython(formData.getString("jython"));
         }
-
+        
         public String getDisplayName() {
             return "Execute Jython script";
         }
-
+        
         @Override
         public String getHelpFile() {
             return "/plugin/jython/help.html";
@@ -63,22 +59,28 @@ public class Jython extends Builder {
     }
 
     @Override
-    public boolean perform(AbstractBuild<?,?> build, Launcher launcher, BuildListener listener)  throws IOException, InterruptedException {
+    public boolean perform(
+            AbstractBuild<?,?> build, Launcher launcher, BuildListener listener)
+            throws IOException, InterruptedException {
         Node builtOn = build.getBuiltOn();
-        boolean master = builtOn == Hudson.getInstance();
         
         JDK configuredJdk = build.getProject().getJDK();
         String javaCmd = configuredJdk != null ?
-            configuredJdk.forNode(builtOn, listener).getHome() + "/bin/java" : "java";
+            configuredJdk.forNode(builtOn, listener).getHome() + "/bin/java" :
+            "java";
         
-        String jythonRuntime = master ?
-            Which.jarFile(jython.class).getAbsolutePath() :
-            builtOn.getRootPath().child("tools/jython/jython-standalone.jar").getRemote();
+        String jythonRuntime =
+            builtOn.getRootPath().child("tools/jython/jython-standalone.jar").
+            getRemote();
+        
+        Map<String,String> envVar =
+            new HashMap<String,String>(build.getEnvironment(listener));
+        envVar.putAll(build.getBuildVariables());
         
         boolean success = 0 == launcher.launch().
             cmds(javaCmd, "-jar", jythonRuntime, "-c", getCommand()).
             masks(false, false, false, false, true).
-            envs(build.getEnvironment(listener)).
+            envs(envVar).
             stdout(listener).
             pwd(build.getWorkspace()).
             join();
