bb455400319254893445e10917ec9911cd47778e
jackgene <jackgene@java.net>
2011-06-17 20:57:28 -0700

    Experimental install logic.

--- src/main/java/org/jvnet/hudson/plugins/PythonPackage.java
+++ src/main/java/org/jvnet/hudson/plugins/PythonPackage.java
@@ -1,7 +1,13 @@
 package org.jvnet.hudson.plugins;
 
+import hudson.remoting.Which;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
+import java.util.Properties;
 import org.kohsuke.stapler.DataBoundConstructor;
+import org.python.util.PythonInterpreter;
+import org.python.util.jython;
 
 /**
  *
@@ -41,8 +47,31 @@ public class PythonPackage {
         return url;
     }
     
-    public void install() {
-        throw new UnsupportedOperationException("not implemented yet");
+    public synchronized void install() {
+        try {
+            Properties props = new Properties();
+            File jythonJar = Which.jarFile(jython.class);
+            String pathSep = System.getProperty("path.separator");
+            props.setProperty(
+                "python.path",
+                jythonJar + "/Lib" + pathSep +
+                jythonJar.getParent() + "/Lib/site-packages" + pathSep +
+                jythonJar.getParent() + "/Lib/site-packages/setuptools-0.6c11-py2.5.egg");
+            PythonInterpreter.initialize(
+                System.getProperties(), props,
+                new String[] {"", "install", getUrl().toString()});
+            PythonInterpreter jython = new PythonInterpreter();
+            jython.exec("import sys");
+            jython.exec("sys.executable = ''");
+            jython.exec("print sys.prefix");
+            jython.exec("print sys.path");
+            jython.exec("print sys.argv");
+            jython.exec("import pip");
+            jython.exec("sys.exit(pip.main())");
+            jython.cleanup();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
     
     public void remove() {
